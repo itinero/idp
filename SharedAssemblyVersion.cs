19a8e1d9649d30bf90f14200688760bc0f67a0c0
Ben Abelshausen <ben@osmsharp.com>
2017-07-10 20:36:07 +0200

    Updated Itinero.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -22,5 +22,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("2.12.0")] // semantic versioning Major.Minor.Patch.Build (9999 will be updated by CI server)
-[assembly: AssemblyInformationalVersion("2.12.0")] // do not change this; build server update this automatically (.9999 will be updated by CI server with -{buildnumber})
+[assembly: AssemblyVersion("2.13.0")] // semantic versioning Major.Minor.Patch.Build (9999 will be updated by CI server)
+[assembly: AssemblyInformationalVersion("2.13.0")] // do not change this; build server update this automatically (.9999 will be updated by CI server with -{buildnumber})
