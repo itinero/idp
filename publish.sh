6c1fd1a4c9d125b4e1a8fe306443cec964344159
xivk <ben@itinero.tech>
2018-07-16 11:57:51 +0200

    Updated build scripts.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
+#!/usr/bin/env bash
 cd ./src/IDP/
 dotnet restore
 dotnet publish -c release -r osx.10.11-x64
-dotnet publish -c release -r ubuntu.16.04-x64
+dotnet publish -c release -r linux-x64
 dotnet publish -c release -r win8-x64
 dotnet publish -c release -r win10-x64
